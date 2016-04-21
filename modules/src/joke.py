5da28d547f4d4720e74e5369a790134a2eb74936
Yash Mittal <yashmittal2009@gmail.com>
2016-04-21 18:12:21 -0400

    Some diversity please (joke)

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -1,13 +1,17 @@
 import re
 import requests
+from random import randint
 
 def process(input, entities=None):
     output = {}
     try:
-        r = requests.get('https://api.chucknorris.io/jokes/random')
-        data = r.json()
         output['input'] = input
-        output['output'] = data['value']
+        if randint(0,1):
+            data = requests.get('https://api.chucknorris.io/jokes/random').json()
+            output['output'] = data['value']
+        else:
+            data = requests.get('http://api.yomomma.info/').json()
+            output['output'] = data['joke']
         output['success'] = True
     except:
         output['success'] = False

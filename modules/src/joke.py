03ecc49fd3573dbe742d3a8cc48443e5aee0d82a
Havan <havanagrawal@gmail.com>
2016-04-23 00:26:59 +0530

    Add another API for jokes

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -1,13 +1,21 @@
 import re
 import requests
+import random
 
 def process(input, entities=None):
     output = {}
+    joke_api_list = ['http://tambal.azurewebsites.net/joke/random',
+                     'https://api.chucknorris.io/jokes/random']
+    joke_key_list = ['joke', 'value']
+    api_count = len(joke_api_list)
+
     try:
-        r = requests.get('https://api.chucknorris.io/jokes/random')
+        index = random.randint(0, api_count - 1)
+        r = requests.get(joke_api_list[index])
+        data_key = joke_key_list[index]
         data = r.json()
         output['input'] = input
-        output['output'] = data['value']
+        output['output'] = data[data_key]
         output['success'] = True
     except:
         output['success'] = False

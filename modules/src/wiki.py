4adf170259ac069807599c21a060fa28f5d478c6
Yash Mittal <yashmittal2009@gmail.com>
2016-04-23 02:21:26 -0400

    Wiki module

--- modules/src/wiki.py
+++ modules/src/wiki.py
@@ -1,14 +1,10 @@
-import sys
-import os
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', '..', 'lib'))
 import wikipedia
 
 def process(input, entities):
     output = {}
     try:
-        query = entities['query'][0]['value']
-        data = wikipedia.summary(query, sentences=0, auto_suggest=True)
+        query = entities['wikipedia_search_query'][0]['value']
+        data = wikipedia.summary(query, sentences=5, auto_suggest=True)
         output['input'] = input
         output['output'] = 'Summary :' + data
         output['success'] = True

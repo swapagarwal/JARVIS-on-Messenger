c28f576f9c3098e2d7243762cc360cb3c4c1b763
eddie <ed.aponte0@gmail.com>
2016-04-23 01:29:11 -0400

    Worked on code suggestions

--- modules/src/weather.py
+++ modules/src/weather.py
@@ -5,11 +5,12 @@ def process(input, entities=None):
     output = {}
 
     try:
-        city = entities['city'][0]['value']
-        r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=adee94563461cdf1dcfe25956aea10ae')
+        location = entities['location'][0]['value']
+        r = requests.get('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=adee94563461cdf1dcfe25956aea10a')
         data = r.json()
         output['input'] = input
-        output['output'] = 'Current weather: ' + data['main.temp']
+        output['output'] = 'Current weather: ' + data['main']['temp'] + data['weather'][0]['description'] + \
+        " information provided by OpenWeatherMap"
         output['success'] = True
     except:
         output['success'] = False

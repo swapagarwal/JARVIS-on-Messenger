4337f18cbc3b802c12c069715b4d665fd7751bcb
Havan <havanagrawal@gmail.com>
2016-04-23 14:58:22 +0530

    Replace joke api

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -1,21 +1,13 @@
 import re
 import requests
-import random
 
 def process(input, entities=None):
     output = {}
-    joke_api_list = ['http://tambal.azurewebsites.net/joke/random',
-                     'https://api.chucknorris.io/jokes/random']
-    joke_key_list = ['joke', 'value']
-    api_count = len(joke_api_list)
-
     try:
-        index = random.randint(0, api_count - 1)
-        r = requests.get(joke_api_list[index])
-        data_key = joke_key_list[index]
+        r = requests.get('http://tambal.azurewebsites.net/joke/random')
         data = r.json()
         output['input'] = input
-        output['output'] = data[data_key]
+        output['output'] = data['joke']
         output['success'] = True
     except:
         output['success'] = False

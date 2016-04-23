27430be23e425d40fb809c71dc69c182c5be055b
Advith Chelikani <advith.chelikani@gmail.com>
2016-04-23 16:31:51 -0700

    added url for wikipedia page

--- modules/src/wiki.py
+++ modules/src/wiki.py
@@ -4,9 +4,10 @@ def process(input, entities):
     output = {}
     try:
         query = entities['wikipedia_search_query'][0]['value']
+        page = wikipedia.page(query)
         data = wikipedia.summary(query, sentences=1)
         output['input'] = input
-        output['output'] = 'Wikipedia summary: ' + data
+        output['output'] = 'Wikipedia summary: ' + data + "\n URL: " + page.url
         output['success'] = True
     except:
         output['success'] = False

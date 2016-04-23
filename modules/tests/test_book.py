6732e9579d62beb7ac467d9e82d1c4d36e12c9d1
Havan <havanagrawal@gmail.com>
2016-04-23 09:53:15 +0530

    Add test for book

---
+++ modules/tests/test_book.py
@@ -0,0 +1,7 @@
+import modules
+
+def test_book():
+    assert('book' == modules.process_query('book timeline')[0])
+    assert('book' == modules.process_query('have you read harry potter?')[0])
+    assert('book' == modules.process_query('little women book rating')[0])
+    assert('book' != modules.process_query('harry potter movie')[0])

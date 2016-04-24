883168602b4e79cafb0ede142f38551c9a6132af
Havan <havanagrawal@gmail.com>
2016-04-24 11:07:18 +0530

    Update currency tests

--- modules/tests/test_currency.py
+++ modules/tests/test_currency.py
@@ -5,3 +5,7 @@ def test_currency():
     assert('currency' == modules.process_query('USD to INR')[0])
     assert('currency' == modules.process_query('how much is USD in EUR')[0])
     assert('currency' != modules.process_query('something random')[0])
+
+    assert('currency' == modules.process_query('convert 5 HKD to USD')[0])
+    assert('currency' == modules.process_query('convert 100 USD to INR')[0])
+    assert('currency' == modules.process_query('how much is 50 USD in EUR')[0])

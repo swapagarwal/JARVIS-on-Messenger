3de7e8617754cd10ec9ea9b5f29d48b5f97460dc
antixcode6 <ed.aponte0@gmail.com>
2016-04-24 11:38:14 -0400

    Update movie.py

--- modules/src/movie.py
+++ modules/src/movie.py
@@ -1,15 +1,1 @@
-import re
-import requests
 
-def process(input, entities):
-    output = {}
-    try:
-        movie = entities['movie'][0]['value']
-        r = requests.get('http://www.omdbapi.com/?t=' + movie + '&plot=short&r=json')
-        data = r.json()
-        output['input'] = input
-        output['output'] = data['Title'] + '\nPlot: ' + data['Plot'] + '\nIMDb Rating: ' + data['imdbRating'] + data['Year']
-        output['success'] = True
-    except:
-        output['success'] = False
-    return output

f2232f7264b7c65bb5d901864de300e72a39fc9b
antixcode6 <ed.aponte0@gmail.com>
2016-04-24 11:37:32 -0400

    Update test_weather.py

--- modules/tests/test_weather.py
+++ modules/tests/test_weather.py
@@ -1,7 +1,7 @@
 import modules
 
 def test_weather():
-    assert('weather' == modules.process_query('tell me the weather in city')[0])
-    assert('weather' == modules.process_query('weather city')[0])
-    assert('weather' == modules.process_query('whats the weather in city')[0])
+    assert('weather' == modules.process_query('tell me the weather in london')[0])
+    assert('weather' == modules.process_query('weather london')[0])
+    assert('weather' == modules.process_query('whats the weather in london')[0])
     assert('weather' != modules.process_query('something random')[0])

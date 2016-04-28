95200d04de53086ba6651f719960f3707caa24db
Eddie <ed.aponte0@gmail.com>
2016-04-27 21:18:03 -0400

    Delete test_weather.py

--- modules/src/test_weather.py
+++
@@ -1,7 +0,0 @@
-import modules
-
-def test_weather():
-    assert('weather' == modules.process_query('tell me the weather in London')[0])
-    assert('weather' == modules.process_query('weather London')[0])
-    assert('weather' == modules.process_query('whats the weather in London')[0])
-    assert('weather' != modules.process_query('something random')[0])

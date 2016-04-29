d63d3c8c847d5e6e7a1aeae3e5864faa24437408
sasek <sasek333@gmail.com>
2016-04-29 15:43:56 +0200

    fix tests

--- modules/tests/test_ping.py
+++ modules/tests/test_ping.py
@@ -2,7 +2,6 @@ import modules
 
 def test_ping():
     assert ('ping' == modules.process_query('ping google.com')[0])
-    assert ('ping' == modules.process_query('check google.com')[0])
     assert ('ping' == modules.process_query('ping')[0])
     assert ('ping' != modules.process_query('something random')[0])
 

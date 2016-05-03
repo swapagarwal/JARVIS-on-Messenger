66bbcdaba5353c3a92d43745320007209314a37d
Advith Chelikani <advith.chelikani@gmail.com>
2016-05-02 18:19:01 -0700

    gives sentence summary and url

--- modules/src/wiki.py
+++ modules/src/wiki.py
@@ -1,13 +1,19 @@
 import wikipedia
+import string
 from templates.text import TextTemplate
 
 def process(input, entities):
     output = {}
     try:
         query = entities['wikipedia_search_query'][0]['value']
-        data = wikipedia.summary(query)
+        data = wikipedia.page(query)
+        template = TextTemplate()
+        printable = set(string.printable)
+        s = filter(lambda x : x in printable, data.summary)
+        template.set_text(s)
+        template.set_post_text(data.url)
         output['input'] = input
-        output['output'] = TextTemplate('Wikipedia Summary: ' + data).get_message()
+        output['output'] = template.get_message()["text"]
         output['success'] = True
     except:
         output['success'] = False

9eaeb3a5918d89142061936666d13ee929515389
Advith Chelikani <advith.chelikani@gmail.com>
2016-05-04 11:38:41 -0700

    remove unnecessary formatting

--- modules/src/wiki.py
+++ modules/src/wiki.py
@@ -1,5 +1,4 @@
 import wikipedia
-import string
 from templates.text import TextTemplate
 
 def process(input, entities):
@@ -8,13 +7,12 @@ def process(input, entities):
         query = entities['wikipedia_search_query'][0]['value']
         data = wikipedia.page(query)
         template = TextTemplate()
-        printable = set(string.printable)
-        s = filter(lambda x : x in printable, data.summary)
-        template.set_text(s)
+        template.set_text(data.summary)
         template.set_post_text(data.url)
         output['input'] = input
-        output['output'] = template.get_message()["text"]
+        output['output'] = template.get_message()
         output['success'] = True
     except:
         output['success'] = False
+        
     return output

73757ad9dc54b0fc9baa91724ff593e1de517a18
Advith Chelikani <advith.chelikani@gmail.com>
2016-05-04 12:36:16 -0700

    Close #16 (#20)
    * added url for wikipedia page
    
    * Merge remote-tracking branch 'swapagarwal/master' into wikipedia
    
    * gives sentence summary and url
    
    * remove unnecessary formatting

--- modules/src/wiki.py
+++ modules/src/wiki.py
@@ -5,10 +5,14 @@ def process(input, entities):
     output = {}
     try:
         query = entities['wikipedia_search_query'][0]['value']
-        data = wikipedia.summary(query)
+        data = wikipedia.page(query)
+        template = TextTemplate()
+        template.set_text(data.summary)
+        template.set_post_text(data.url)
         output['input'] = input
-        output['output'] = TextTemplate('Wikipedia Summary: ' + data).get_message()
+        output['output'] = template.get_message()
         output['success'] = True
     except:
         output['success'] = False
+        
     return output

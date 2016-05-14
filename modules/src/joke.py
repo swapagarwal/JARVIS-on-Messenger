127f23349c1cc778d2319a351fe75b8d42ae85b7
Surya Teja K <shanmukhateja@gmail.com>
2016-05-14 11:34:22 +0530

    Remove API call in joke module (#35)
    See #4
    
    * improve jokes module
    
    * migrated to json format

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -1,14 +1,27 @@
 import requests
 from templates.text import TextTemplate
+from random import choice
+import json
+import config
 
 def process(input, entities=None):
     output = {}
     try:
+        '''
         r = requests.get('http://tambal.azurewebsites.net/joke/random')
         data = r.json()
         output['input'] = input
         output['output'] = TextTemplate(data['joke']).get_message()
-        output['success'] = True
+        output['success'] = True        
+        '''
+        
+        with open(config.JOKES_SOURCE_FILE,"r") as file:
+            dump = json.load(file) # read the whole file
+            jokes_list = dump.get("jokes")
+            output['input'] = input
+            joke = choice(jokes_list).strip() #randomly choose a joke
+            output['output'] = TextTemplate(joke).get_message()
+            output['success']=True
     except:
         output['success'] = False
     return output

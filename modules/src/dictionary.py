6b9fbbf4281d4401e7e6adc0d21d877e35543f9c
Akarsh Hegde <akarshhegde340@gmail.com>
2016-05-15 00:35:26 +0530

    New dictionary api -

--- modules/src/dictionary.py
+++ modules/src/dictionary.py
@@ -5,10 +5,11 @@ def process(input, entities):
     output = {}
     try:
         word = entities['word'][0]['value']
-        r = requests.get('https://glosbe.com/gapi/translate?from=eng&dest=eng&format=json&phrase=' + word + '&pretty=true')
+        token = os.environ.get('WORDS_API_KEY')
+        r = requests.get('https://wordsapiv1.p.mashape.com/words/' + word + '/definitions', headers = {'X-Mashape-Key':token})	
         data = r.json()
         output['input'] = input
-        output['output'] = TextTemplate('Definition of ' + word + ':\n' + data['tuc'][0]['meanings'][0]['text']).get_message()
+        output['output'] = TextTemplate('Definition of ' + word + ':\n' + data['definitions']['definition']).get_message()
         output['success'] = True
     except:
         output['success'] = False

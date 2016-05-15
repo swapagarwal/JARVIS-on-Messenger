b35694be5d1b5f977201b8190ac4ea5326471f21
Akarsh Hegde <akarshhegde340@gmail.com>
2016-05-15 11:35:08 +0530

    Update test_anime.py

--- modules/tests/test_anime.py
+++ modules/tests/test_anime.py
@@ -2,6 +2,6 @@ import modules
 
 def test_anime():
     assert('anime' == modules.process_query('Death Note anime')[0])
-    assert('anime' == modules.process_query('Dragon ball super status')[0])
-    assert('anime' == modules.process_query('What is the rating of One Punch Man?')[0])
+    assert('anime' == modules.process_query('Dragon ball super anime status')[0])
+    assert('anime' == modules.process_query('What is the anime rating of One Punch Man?')[0])
     assert('anime' != modules.process_query('something random')[0])

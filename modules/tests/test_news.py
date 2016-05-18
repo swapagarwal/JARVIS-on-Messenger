662b3fd8861b8cf6945b721b94e2786a24cd4f09
Raymond Akornor <raymondakornor@gmail.com>
2016-05-18 21:32:55 +0000

    fixed bug in test_news

--- modules/tests/test_news.py
+++ modules/tests/test_news.py
@@ -4,4 +4,4 @@ def test_news():
 	assert('news' == modules.process_query("United States Election")[0])
 	assert('news' == modules.process_query("What's happening around the world")[0])
 	assert('news' == modules.process_query("What's the latest on fashion news")[0])
-	assert('news' == modules.process_query("something random")[0])
+	assert('news' != modules.process_query("something random")[0])

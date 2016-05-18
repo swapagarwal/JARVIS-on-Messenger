a1cd053bc27a2904aad6e83093b475a5f020e493
Raymond Akornor <raymondakornor@gmail.com>
2016-05-18 22:05:05 +0000

    might work

--- modules/tests/test_news.py
+++ modules/tests/test_news.py
@@ -1,7 +1,7 @@
 import modules
 
 def test_news():
-	assert('news' == modules.process_query("United States Election")[0])
+	assert('news' == modules.process_query("what's the latest on the facebook case")[0])
 	assert('news' == modules.process_query("What's happening around the world")[0])
 	assert('news' == modules.process_query("What's the latest on fashion news")[0])
 	assert('news' != modules.process_query("something random")[0])

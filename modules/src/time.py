32fe04fb9f546f6fa878390e984682876c12c7ff
Swapnil Agarwal <swapnilagarwal@live.com>
2016-06-06 04:58:25 +0000

    Format time module

--- modules/src/time.py
+++ modules/src/time.py
@@ -4,21 +4,20 @@ import os
 from templates.text import TextTemplate
 from datetime import datetime
 
-MAPQUEST_ACCESS_TOKEN = os.environ.get('MAPQUEST_ACCESS_TOKEN', config.MAPQUEST_ACCESS_TOKEN)
-TIME_ZONE_ACCESS_TOKEN = os.environ.get('TIME_ZONE_ACCESS_TOKEN', config.TIME_ZONE_ACCESS_TOKEN)
+MAPQUEST_CONSUMER_KEY = os.environ.get('MAPQUEST_CONSUMER_KEY', config.MAPQUEST_CONSUMER_KEY)
+TIME_ZONE_DB_API_KEY = os.environ.get('TIME_ZONE_DB_API_KEY', config.TIME_ZONE_DB_API_KEY)
 
 def process(input, entities):
     output = {}
     try:
-        l = requests.get('http://open.mapquestapi.com/nominatim/v1/search.php?key=' + MAPQUEST_ACCESS_TOKEN + '&format=json&q='+ entities['time'][0]['value'] + '&addressdetails=0&limit=1')
-        location = l.json()
-        r = requests.get('http://api.timezonedb.com/?lat='+ location[0]['lat'] + '&lng='+ location[0]['lon'] + '&format=json&key=' + TIME_ZONE_ACCESS_TOKEN)
+        r = requests.get('http://open.mapquestapi.com/nominatim/v1/search.php?key=' + MAPQUEST_CONSUMER_KEY + '&format=json&q='+ entities['location'][0]['value'] + '&limit=1')
+        location_data = r.json()
+        r = requests.get('http://api.timezonedb.com/?lat='+ location_data[0]['lat'] + '&lng='+ location_data[0]['lon'] + '&format=json&key=' + TIME_ZONE_DB_API_KEY)
         time_data = r.json()
-        the_time = datetime.utcfromtimestamp(time_data['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
+        time = datetime.utcfromtimestamp(time_data['timestamp']).strftime('%Y-%m-%d %H:%M:%S')
         output['input'] = input
-        output['output'] = TextTemplate('Location: ' + location[0]['display_name'] + '\nTime: ' + the_time + '\nTime Zone: ' + time_data['abbreviation']).get_message()
+        output['output'] = TextTemplate('Location: ' + location_data[0]['display_name'] + '\nTime: ' + time + '\nTime Zone: ' + time_data['abbreviation']).get_message()
         output['success'] = True
     except:
         output['success'] = False
-
     return output

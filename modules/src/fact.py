36b67184c981a5925955292c408f4019d2b2f4a4
Swapnil Agarwal <swapnilagarwal@live.com>
2016-06-09 16:45:11 +0000

    Use offline data sources for facts and quotes
    Close #54

--- modules/src/fact.py
+++ modules/src/fact.py
@@ -1,9 +1,13 @@
 import requests
 from templates.text import TextTemplate
+from random import choice
+import json
+import config
 
 def process(input, entities=None):
     output = {}
     try:
+        '''
         r = requests.get('http://numbersapi.com/random/trivia', headers={
             'Content-Type': 'application/json'
         })
@@ -11,6 +15,13 @@ def process(input, entities=None):
         output['input'] = input
         output['output'] = TextTemplate(data['text']).get_message()
         output['success'] = True
+        '''
+        with open(config.FACTS_SOURCE_FILE) as facts_file:
+            facts = json.load(facts_file)
+            facts_list = facts['facts']
+            output['input'] = input
+            output['output'] = TextTemplate(choice(facts_list)).get_message()
+            output['success'] = True
     except:
         output['success'] = False
     return output
--- modules/src/quote.py
+++ modules/src/quote.py
@@ -1,15 +1,26 @@
 import requests
 from templates.text import TextTemplate
+from random import choice
+import json
+import config
 
 def process(input, entities=None):
     output = {}
     try:
+        '''
         # Programming quotes
         r = requests.get('http://quotes.stormconsultancy.co.uk/random.json')
         data = r.json()
         output['input'] = input
         output['output'] = TextTemplate(data['quote'] + ' - ' + data['author']).get_message()
         output['success'] = True
+        '''
+        with open(config.QUOTES_SOURCE_FILE) as quotes_file:
+            quotes = json.load(quotes_file)
+            quotes_list = quotes['quotes']
+            output['input'] = input
+            output['output'] = TextTemplate(choice(quotes_list)).get_message()
+            output['success'] = True
     except:
         output['success'] = False
     return output

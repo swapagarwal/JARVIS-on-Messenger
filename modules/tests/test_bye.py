ccb660b7978479aaf90e1bc8b27966ab57bd0148
Swapnil Agarwal <swapnilagarwal@live.com>
2016-07-11 14:35:30 +0000

    Format bye module

--- modules/tests/test_bye.py
+++ modules/tests/test_bye.py
@@ -2,5 +2,5 @@ import modules
 
 def test_bye():
     assert('bye' == modules.process_query('bye')[0])
-    assert('bye' == modules.process_query('byebye')[0])
-    assert('bye' != modules.process_query('good time')[0])
+    assert('bye' == modules.process_query('good night')[0])
+    assert('bye' != modules.process_query('something random')[0])

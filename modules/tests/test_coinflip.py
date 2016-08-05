f6213e60c0286c993a2260a799f281f3426e7b45
Seong Ju Park <seongju@umich.edu>
2016-08-05 10:19:15 -0400

    Fixed the typo error in test_coinflip.py

--- modules/tests/test_coinflip.py
+++ modules/tests/test_coinflip.py
@@ -1,7 +1,7 @@
 import modules
 
 def test_coinflip():
-        assert('coinflip' == modules.process_query('Flip a coin')[0]
-        assert('coinflip' == modules.process_query('Jarvis flip a coin')[0]
-        assert('coinflip' == modules.process_query('Can you flip a coin')[0]
-        assert('coinflip' == modules.process_query('something random')[0]
+        assert('coinflip' == modules.process_query('Flip a coin'))[0]
+        assert('coinflip' == modules.process_query('Jarvis flip a coin'))[0]
+        assert('coinflip' == modules.process_query('Can you flip a coin'))[0]
+        assert('coinflip' == modules.process_query('something random'))[0]

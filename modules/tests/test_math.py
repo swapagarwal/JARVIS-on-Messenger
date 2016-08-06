bc32bb2fb7e4dcb93fb257ecce295090e860e88c
John Hamlett <hamle010@umn.edu>
2016-08-06 11:31:50 -0500

    Added tests for math module

---
+++ modules/tests/test_math.py
@@ -0,0 +1,6 @@
+import modules
+
+def test_math():
+    assert('math' == modules.process_query('Can you solve 2 + 2')[0])
+    assert('math' == modules.process_query('Can you evaluate 2 + 2')[0])
+    assert('math' == modules.process_query('What is 2 + 2')[0])

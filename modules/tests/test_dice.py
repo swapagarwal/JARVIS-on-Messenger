d296f45b2cb4048b7c87dd0174f47768594fee34
Ushal Naidoo <ushal7naidoo@gmail.com>
2016-08-09 09:44:07 +1200

    renamed test module

--- modules/tests/test_dice.py
+++ modules/tests/test_dice.py
@@ -1,6 +1,6 @@
 import modules
 
-def test_diceroll():
+def test_dice():
   assert('dice' == modules.process_query('Roll a dice')[0])
   assert('dice' == modules.process_query('Jarvis, roll a dice')[0])
   assert('dice' == modules.process_query('Can you roll a dice')[0])

4ef8e6807c4e4252ef527617f392237a4dc07181
Ushal Naidoo <ushal7naidoo@gmail.com>
2016-08-10 13:15:51 +1200

    Added Personalized Hello,Bye and Help Messages

--- modules/src/bye.py
+++ modules/src/bye.py
@@ -2,11 +2,16 @@ import random
 from templates.text import TextTemplate
 
 def process(input, entities=None):
+    sender_name = 'sir'
+    if entities is not None:
+        if 'sender' in entities:
+            sender_name = entities['first_name']
+
     greetings = [
-        'Have a good day, sir.',
-        'Wonderful, I think it\'s time for my evening nap...',
-        'Bye to you as well, good sir.',
-        'It was my pleasure talking to you.',
+        'Have a good day, ' + sender_name + '.',
+        'Wonderful ' + sender_name + ', I think it\'s time for my evening nap...',
+        'Bye to you as well, ' + sender_name + '.',
+        'It was my pleasure talking to you, ' + sender_name,
         'Oh, please do not go!',
     ]
     output = {
--- modules/src/hello.py
+++ modules/src/hello.py
@@ -2,13 +2,18 @@ import random
 from templates.text import TextTemplate
 
 def process(input, entities=None):
+    sender_name = 'sir'
+    if entities is not None:
+        if 'sender' in entities:
+            sender_name = entities['first_name']
+
     greetings = [
-        'Welcome home, sir...',
-        'All wrapped up here, sir. Will there be anything else?',
-        'Sir, I think I need to sleep now...',
+        'Welcome home, ' + sender_name + '...',
+        'All wrapped up here, ' + sender_name + '. Will there be anything else?',
+        sender_name + ', I think I need to sleep now...',
         'I seem to do quite well for a stretch, and then at the end of the sentence I say the wrong cranberry.',
-        'At your service, sir.',
-        'Oh hello, sir!',
+        'At your service, ' + sender_name + '.',
+        'Oh hello, ' + sender_name + '!',
         'Perhaps, if you intend to visit other planets, we should improve the exosystems.',
     ]
     output = {

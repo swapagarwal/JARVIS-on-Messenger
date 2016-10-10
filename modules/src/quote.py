2a2124cb4009335d2b656aa28d9cd9f2c805315f
Netanel Malka <netanel246@gmail.com>
2016-10-10 23:57:02 +0300

    Replace the API web site to more generic quotes service(Instead of design quotes). Also made the code more simpler

--- modules/src/quote.py
+++ modules/src/quote.py
@@ -1,25 +1,20 @@
-from requests import get
+from requests import post
 from templates.text import TextTemplate
-from re import sub
-from HTMLParser import HTMLParser
 
 
 def process(input, entities=None):
     output = {}
     try:
-        h = HTMLParser()
-        res = get('http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1')
-        remove_html_tags_pattern = r'(<\w>|</\w>)'
-        data = res.json()[0]
-
-        # Remove html paragraph tags. h.unescape() not remove them
-        content = sub(remove_html_tags_pattern, "", data["content"])
-
-        # Remove some HTML tags from the response
-        escaped_html_content = h.unescape(content)
-        author = data['title']
+        send_data = {
+            'method': 'getQuote',
+            'format': 'json',
+            'lang': "en",
+            'key': ""
+        }
+        res = post('http://api.forismatic.com/api/1.0/', send_data)
+        content = res.json()
         output['input'] = input
-        output['output'] = TextTemplate("{0} - {1}".format(escaped_html_content, author)).get_message()
+        output['output'] = TextTemplate("{0} - {1}".format(content["quoteText"], content["quoteAuthor"])).get_message()
         output['success'] = True
 
     except:

23e05f35a66d27799a87f85916c7ae7d0a0ff570
Vinod  Vanga <vinod.vanga@imaginea.com>
2016-10-26 16:47:20 +0530

    Changed hello message

--- modules/src/hello.py
+++ modules/src/hello.py
@@ -4,12 +4,6 @@ from templates.text import TextTemplate
 def process(input, entities=None):
     greetings = [
         'Welcome home, sir...',
-        'All wrapped up here, sir. Will there be anything else?',
-        'Sir, I think I need to sleep now...',
-        'I seem to do quite well for a stretch, and then at the end of the sentence I say the wrong cranberry.',
-        'At your service, sir.',
-        'Oh hello, sir!',
-        'Perhaps, if you intend to visit other planets, we should improve the exosystems.',
     ]
     output = {
         'input': input,

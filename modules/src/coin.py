63407cb0de9abaacf27243774381f82b068644f0
Swapnil Agarwal <swapnilagarwal@live.com>
2016-11-01 18:53:03 +0000

    Format coin module

--- modules/src/coin.py
+++ modules/src/coin.py
@@ -1,12 +1,18 @@
+import json
 import random
 from templates.text import TextTemplate
+from templates.quick_replies import add_quick_reply
 
-def process(input, entities):
-    output = {}
-    output['input'] = input
-    if random.randrange(0,2,1):
-            output['output'] = TextTemplate("Sure, it's heads!").get_message()
-    else:
-            output['output'] = TextTemplate("Sure, it's tails!").get_message()
-    output['success'] = True
+def process(input, entities=None):
+    toss = TextTemplate(random.choice(['heads', 'tails'])).get_message()
+    postback = {
+        'intent': 'coin',
+        'entities': None
+    }
+    message = add_quick_reply(toss, 'Flip again!', json.dumps(postback))
+    output = {
+        'input': input,
+        'output': message,
+        'success': True
+    }
     return output
--- modules/tests/test_coin.py
+++ modules/tests/test_coin.py
@@ -1,7 +1,7 @@
 import modules
 
-def test_coinflip():
-        assert('coin' == modules.process_query('Flip a coin')[0])
-        assert('coin' == modules.process_query('Jarvis flip a coin')[0])
-        assert('coin' == modules.process_query('Can you flip a coin')[0])
-        assert('coin' != modules.process_query('something random')[0])
+def test_coin():
+    assert('coin' == modules.process_query('Flip a coin')[0])
+    assert('coin' == modules.process_query('heads or tails')[0])
+    assert('coin' == modules.process_query('Can you flip a coin?')[0])
+    assert('coin' != modules.process_query('something random')[0])

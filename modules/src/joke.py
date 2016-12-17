00406e4b3201e99f1ae1ea54c723dbf3f658dd5b
CoderOwl <c.reva1996@gmail.com>
2016-12-18 02:36:31 +0530

    Adding quote quick reply for joke module

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -8,22 +8,20 @@ import config
 def process(input, entities=None):
     output = {}
     try:
-        '''
-        r = requests.get('http://tambal.azurewebsites.net/joke/random')
-        data = r.json()
-        output['input'] = input
-        output['output'] = TextTemplate(data['joke']).get_message()
-        output['success'] = True
-        '''
         with open(config.JOKES_SOURCE_FILE) as jokes_file:
             jokes = json.load(jokes_file)
             jokes_list = jokes['jokes']
-	    joke = TextTemplate(choice(jokes_list)).get_message()
-	    postback = {
-		'intent': 'joke',
-		'entities': None
-		}
-	    message = add_quick_reply(joke, 'One more!', json.dumps(postback))
+            joke = TextTemplate(choice(jokes_list)).get_message()
+    	    postback_joke = {
+    		'intent': 'joke',
+    		'entities': None
+    		}
+    	    postback_quote = {
+    		'intent': 'quote',
+    		'entities': None
+    		}
+    	    message = add_quick_reply(joke, 'One more!', json.dumps(postback_joke))
+            message = add_quick_reply(message, 'Show me a quote.', json.dumps(postback_quote))
             output['input'] = input
             output['output'] = message
             output['success'] = True

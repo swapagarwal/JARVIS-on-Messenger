67bbb2f5989954a6735dc308b089135e99ecd61f
tdraebing <thomas.draebing@googlemail.com>
2016-12-18 17:55:42 +0100

    - changed out of coins module to images of a US-quarter coin - changed dice module output to unicode symbol of the respective dice sides

--- modules/src/coin.py
+++ modules/src/coin.py
@@ -1,10 +1,14 @@
 import json
 import random
-from templates.text import TextTemplate
+from templates.attachment import AttachmentTemplate
 from templates.quick_replies import add_quick_reply
 
+#images by US Mint; published on wikimedia under public domain rights.
+coin_images = {'heads': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/2006_Quarter_Proof.png/244px-2006_Quarter_Proof.png',
+               'tails': 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Oregon_quarter%2C_reverse_side%2C_2005.jpg/236px-Oregon_quarter%2C_reverse_side%2C_2005.jpg'}
+
 def process(input, entities=None):
-    toss = TextTemplate(random.choice(['heads', 'tails'])).get_message()
+    toss = AttachmentTemplate(random.choice(list(coin_images.values())), type='image').get_message()
     postback = {
         'intent': 'coin',
         'entities': None

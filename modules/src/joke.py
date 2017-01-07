d20df7c3865a2ce8dbdaa6dc5a97f92d8d74219c
CoderOwl <c.reva1996@gmail.com>
2017-01-07 08:18:25 +0530

    Edits according to code review.

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -11,16 +11,16 @@ def process(input, entities=None):
         with open(config.JOKES_SOURCE_FILE) as jokes_file:
             jokes = json.load(jokes_file)
             jokes_list = jokes['jokes']
-            joke = TextTemplate(choice(jokes_list)).get_message()
-    	    postback_joke = {
+            message = TextTemplate(choice(jokes_list)).get_message()
+            postback_joke = {
     		'intent': 'joke',
     		'entities': None
     		}
-    	    postback_quote = {
+            postback_quote = {
     		'intent': 'quote',
     		'entities': None
     		}
-    	    message = add_quick_reply(joke, 'One more!', json.dumps(postback_joke))
+            message = add_quick_reply(message, 'One more!', json.dumps(postback_joke))
             message = add_quick_reply(message, 'Show me a quote.', json.dumps(postback_quote))
             output['input'] = input
             output['output'] = message

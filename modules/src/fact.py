8b9938d78d952d3fb22b90e0dc46ee4c5b5a34e5
Daniel Edades <edadesd@oregonstate.edu>
2017-03-20 14:43:11 -0700

    Created quickReplyFacts branch and started adding quick replies to fact.py

--- modules/src/fact.py
+++ modules/src/fact.py
@@ -19,8 +19,18 @@ def process(input, entities=None):
         with open(config.FACTS_SOURCE_FILE) as facts_file:
             facts = json.load(facts_file)
             facts_list = facts['facts']
+            message = TextTemplate(choice(facts_list)).get_message()
+			
+            postback_fact = {
+			
+				'intent': 'fact',
+				'entities': None
+            }
+			
+			
             output['input'] = input
-            output['output'] = TextTemplate(choice(facts_list)).get_message()
+            message = add_quick_reply(message, 'One more!', json.dumps(postback_fact))
+            output['output'] = message
             output['success'] = True
     except:
         output['success'] = False

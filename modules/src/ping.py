24016d865ed680571a649b5bc80ede9322547aca
Anshumaan Parashar <apairs.anshumaan@gmail.com>
2017-06-15 21:41:50 +0530

    Added the ping module

--- modules/src/ping.py
+++ modules/src/ping.py
@@ -1,5 +1,5 @@
 import requests
-from  bs4 import BeautifulSoup
+import json
 
 from templates.text import TextTemplate
 
@@ -7,14 +7,19 @@ def process(input, entitites):
     output = {}
     try:
         url = entities['url'][0]['value']
+        orig_url = url
         strip_list = ['http://','https://','/']
         for item in strip_list:
             url = url.strip(item)
-        page = requests.get('https://isitup.org/'+str(url))
-        soup = BeautifulSoup(page.content, 'html.parser')
-        txt = soup.find('div', id='container').find('p').text
-        if txt[0]=='W':
-            txt = 'The web domain you entered seems to be invalid!'
+        page = requests.get('https://isitup.org/'+url+'.json')
+        page = json.loads(page.text)
+        status = page['status_code']
+        if status == 1:
+            txt = orig_url + ' is up!'
+        elif status == 2:
+            txt = orig_url + ' seems to be down!'
+        else :
+            txt = 'I need a valid domain to check!'
         output['input'] = input
         output['output'] = TextTemplate(txt).get_message()
         output['success'] = True
@@ -23,6 +28,7 @@ def process(input, entitites):
         error_message = '\nPlease ask me something else, like:'
         error_message = '\n - is google.com up'
         error_message = '\n - google.com status'
+        error_message = '\n - ping google.com'
         output['error_msg'] = TextTemplate(error_message).get_message()
         output['success'] = False
     return output

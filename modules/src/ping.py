95920e26f2987bf0019304770b35be4be8d002e5
Anshumaan Parashar <apairs.anshumaan@gmail.com>
2017-06-17 00:14:06 +0530

    Applied proposed changes

--- modules/src/ping.py
+++ modules/src/ping.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 from templates.text import TextTemplate
 
@@ -8,28 +7,27 @@ def process(input, entitites):
     try:
         url = entities['url'][0]['value']
         orig_url = url
-        strip_list = ['http://','https://','/']
+        strip_list = ['http://', 'https://', '/']
         for item in strip_list:
             url = url.strip(item)
-        page = requests.get('https://isitup.org/'+url+'.json')
-        page = json.loads(page.text)
+        page = requests.get('https://isitup.org/' + url + '.json')
+        page = page.json()
         status = page['status_code']
         if status == 1:
-            txt = orig_url + ' is up!'
+            txt = url + ' is up!'
         elif status == 2:
-            txt = orig_url + ' seems to be down!'
-        else :
+            txt = url + ' seems to be down!'
+        elif status == 3:
             txt = 'I need a valid domain to check!'
         output['input'] = input
         output['output'] = TextTemplate(txt).get_message()
         output['success'] = True
     except:
         error_message = 'There seems to be some problem'
-        error_message = '\nPlease ask me something else, like:'
-        error_message = '\n - is google.com up'
-        error_message = '\n - google.com status'
-        error_message = '\n - ping google.com'
+        error_message += '\nPlease ask me something else, like:'
+        error_message += '\n - is google.com up'
+        error_message += '\n - google.com status'
+        error_message += '\n - ping google.com'
         output['error_msg'] = TextTemplate(error_message).get_message()
         output['success'] = False
     return output
-

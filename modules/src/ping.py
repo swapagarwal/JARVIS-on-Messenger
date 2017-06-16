04508c5a835ebb5eecb7934dd551e8de765ddf01
Anshumaan Parashar <apairs.anshumaan@gmail.com>
2017-06-17 00:32:47 +0530

    Add ping module (#197)
    * Added functionality to check whether a website is running or not
    
    * Add the module name in __init__.py
    
    *  Added the ping module
    
    * Applied proposed changes

---
+++ modules/src/ping.py
@@ -0,0 +1,33 @@
+import requests
+
+from templates.text import TextTemplate
+
+def process(input, entitites):
+    output = {}
+    try:
+        url = entities['url'][0]['value']
+        orig_url = url
+        strip_list = ['http://', 'https://', '/']
+        for item in strip_list:
+            url = url.strip(item)
+        page = requests.get('https://isitup.org/' + url + '.json')
+        page = page.json()
+        status = page['status_code']
+        if status == 1:
+            txt = url + ' is up!'
+        elif status == 2:
+            txt = url + ' seems to be down!'
+        elif status == 3:
+            txt = 'I need a valid domain to check!'
+        output['input'] = input
+        output['output'] = TextTemplate(txt).get_message()
+        output['success'] = True
+    except:
+        error_message = 'There seems to be some problem'
+        error_message += '\nPlease ask me something else, like:'
+        error_message += '\n - is google.com up'
+        error_message += '\n - google.com status'
+        error_message += '\n - ping google.com'
+        output['error_msg'] = TextTemplate(error_message).get_message()
+        output['success'] = False
+    return output

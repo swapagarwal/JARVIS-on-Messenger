0796f1af300142695c40b8675a24ec92a786f981
Lee Haugen <haugenlee@leegenes.local>
2017-06-19 13:25:35 -0400

    improve ping

--- modules/src/ping.py
+++ modules/src/ping.py
@@ -1,5 +1,6 @@
 import requests
 
+from templates.generic import *
 from templates.text import TextTemplate
 from urlparse import urlparse
 
@@ -17,14 +18,19 @@ def process(input, entities):
         status = data['status_code']
         if status == 1:
             text = hostname + ' is up.'
+            image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Arbcom_ru_ready.svg/240px-Arbcom_ru_ready.svg.png'
         elif status == 2:
             text = hostname + ' seems to be down!'
+            image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Arbcom_ru_withdrawn.svg/240px-Arbcom_ru_withdrawn.svg.png'
         elif status == 3:
             text = 'Please enter a valid domain to check availability.'
+            image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Twemoji_2755.svg/240px-Twemoji_2755.svg.png'
         else:
             raise Exception("Something unexpected happened!")
+        template = GenericTemplate()
+        template.add_element(title=text, image_url=image_url)
         output['input'] = input
-        output['output'] = TextTemplate(text).get_message()
+        output['output'] = template.get_message()
         output['success'] = True
     except:
         error_message = 'There seems to be a problem looking up that domain.'

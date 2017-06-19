d46d76df5113626c8ffebb0f9466a4dd325fb4fa
Lucas Renatus <LucasBPM@users.noreply.github.com>
2017-06-19 03:38:53 -0600

    Ping: Handle URLs with path / parameters (#204)
    Fix #203

--- modules/src/ping.py
+++ modules/src/ping.py
@@ -1,21 +1,24 @@
 import requests
 
 from templates.text import TextTemplate
+from urlparse import urlparse
 
 def process(input, entities):
     output = {}
     try:
         url = entities['url'][0]['value']
-        strip_list = ['http://', 'https://', '/']
-        for item in strip_list:
-            url = url.strip(item)
-        r = requests.get('https://isitup.org/' + url + '.json')
+        if not urlparse(url).scheme:
+            url = "https://" + url
+        hostname = urlparse(url).hostname
+        if hostname is None:
+            raise Exception("Please enter a valid hostname to check availability.")
+        r = requests.get('https://isitup.org/' + hostname + '.json')
         data = r.json()
         status = data['status_code']
         if status == 1:
-            text = url + ' is up.'
+            text = hostname + ' is up.'
         elif status == 2:
-            text = url + ' seems to be down!'
+            text = hostname + ' seems to be down!'
         elif status == 3:
             text = 'Please enter a valid domain to check availability.'
         else:

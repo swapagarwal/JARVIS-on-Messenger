a1db0664137e53e6459b196bacb8d36c83ac0ae2
RevanthChetluru <c.reva1996@gmail.com>
2017-06-23 18:50:35 +0530

    Joke: Add quick replies (#140)
    See #131

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -1,5 +1,6 @@
 import requests
 from templates.text import TextTemplate
+from templates.quick_replies import add_quick_reply
 from random import choice
 import json
 import config
@@ -17,8 +18,24 @@ def process(input, entities=None):
         with open(config.JOKES_SOURCE_FILE) as jokes_file:
             jokes = json.load(jokes_file)
             jokes_list = jokes['jokes']
+            message = TextTemplate(choice(jokes_list)).get_message()
+            postback_joke = {
+                'intent': 'joke',
+                'entities': None
+            }
+            postback_quote = {
+                'intent': 'quote',
+                'entities': None
+            }
+            postback_fact = {
+                'intent': 'fact',
+                'entities': None
+            }
+            message = add_quick_reply(message, 'One more!', json.dumps(postback_joke))
+            message = add_quick_reply(message, 'Show me a quote.', json.dumps(postback_quote))
+            message = add_quick_reply(message, 'Tell me a fact.', json.dumps(postback_fact))
             output['input'] = input
-            output['output'] = TextTemplate(choice(jokes_list)).get_message()
+            output['output'] = message
             output['success'] = True
     except:
         output['success'] = False

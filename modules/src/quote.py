fb6ebf637948c0606b9a16510e2046e3daa1498b
Chanond Wongpiya <chanondw@gmail.com>
2017-06-25 18:21:53 +0700

    Quote: Add quick replies (#211)
    See #131

--- modules/src/quote.py
+++ modules/src/quote.py
@@ -1,5 +1,6 @@
 import requests
 from templates.text import TextTemplate
+from templates.quick_replies import add_quick_reply
 from random import choice
 import json
 import config
@@ -18,8 +19,24 @@ def process(input, entities=None):
         with open(config.QUOTES_SOURCE_FILE) as quotes_file:
             quotes = json.load(quotes_file)
             quotes_list = quotes['quotes']
+            message = TextTemplate(choice(quotes_list)).get_message()
+            postback_quote = {
+                'intent': 'quote',
+                'entities': None
+            }
+            postback_fact = {
+                'intent': 'fact',
+                'entities': None
+            }
+            postback_joke = {
+                'intent': 'joke',
+                'entities': None
+            }
+            message = add_quick_reply(message, 'Another one!', json.dumps(postback_quote))
+            message = add_quick_reply(message, 'Show me a fact.', json.dumps(postback_fact))
+            message = add_quick_reply(message, 'Tell me a joke.', json.dumps(postback_joke))
             output['input'] = input
-            output['output'] = TextTemplate(choice(quotes_list)).get_message()
+            output['output'] = message
             output['success'] = True
     except:
         output['success'] = False

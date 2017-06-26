81dfb6cd72cb02215f5d8ecc963f498775973731
Abraham Albert <abrahamalbert18@icloud.com>
2017-06-27 00:05:12 +0530

    Update dice.py
    Whenever you're asking JARVIS to roll a die, it's just replying with unicode character of dice in python. The problem here the character looks so small on larger resolution desktops or laptops or mobiles.

--- modules/src/dice.py
+++ modules/src/dice.py
@@ -3,14 +3,12 @@ import random
 from templates.text import TextTemplate
 from templates.quick_replies import add_quick_reply
 
-dice_sides = {
-	1: u'\u2680',
-	2: u'\u2681',
-	3: u'\u2682',
-	4: u'\u2683',
-	5: u'\u2684',
-	6: u'\u2685'
-}
+dice_sides = {1: 'https://upload.wikimedia.org/wikipedia/commons/6/62/Kismet-Ace.png',
+              2: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Kismet-Deuce.png',
+              3: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Kismet-Trey.png',
+              4: 'https://upload.wikimedia.org/wikipedia/commons/1/11/Kismet-Four.png',
+              5: 'https://upload.wikimedia.org/wikipedia/commons/8/8e/Kismet-Five.png',
+              6: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/Kismet-Six.png'}
 
 def process(input, entities=None):
     roll = TextTemplate(dice_sides[random.randint(1, 6)]).get_message()

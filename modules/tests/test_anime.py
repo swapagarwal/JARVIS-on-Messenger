707dce642b38d9e6f0a87afd8260b1fb98583bd7
Daniel Edades <edadesd@oregonstate.edu>
2017-06-28 19:15:07 -0700

    Added more tests to test_anime

--- modules/tests/test_anime.py
+++ modules/tests/test_anime.py
@@ -1,4 +1,5 @@
 import modules
+import json
 
 
 def test_anime():
@@ -6,3 +7,43 @@ def test_anime():
     assert ('anime' == modules.process_query('Dragon ball super anime status')[0])
     assert ('anime' == modules.process_query('What is the anime rating of One Punch Man?')[0])
     assert ('anime' != modules.process_query('something random')[0])
+
+    response = modules.search('Death Note anime')
+    response_payload = response['attachment']['payload']
+    
+    # Test that a payload was returned as a string
+
+    assert(type(response_payload['text']) is str 
+            or type(response_payload is unicode))
+
+    # Test that the expected title was returned
+
+    assert(response_payload['text'][:17] == 'Title: Death Note')
+
+    # Test that an average rating was returned
+
+    assert('Average Rating: ' in response_payload['text'])
+
+    # Test that a popularity rank was returned
+
+    assert('Popularity Rank: ' in response_payload['text'])
+
+    # Test that an episode count was returned
+
+    assert('Episode Count: ' in response_payload['text'])
+
+    payload_buttons = response_payload['buttons']
+
+    # Test that a Kitsu URL was returned
+
+    assert('url' in payload_buttons[0].keys())
+    assert('https://kitsu.io' in payload_buttons[0]['url'])
+    assert('title' in payload_buttons[0].keys())
+    assert(payload_buttons[0]['title'] == "Kitsu URL")
+
+    # Test that a YouTube URL was returned
+
+    assert('url' in payload_buttons[1].keys())
+    assert('https://www.youtube.com' in payload_buttons[1]['url'])
+    assert('title' in payload_buttons[1].keys())
+    assert(payload_buttons[1]['title'] == "YouTube URL")

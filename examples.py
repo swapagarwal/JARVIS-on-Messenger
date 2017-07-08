f1ba941ede457344bc201181eac9b71b053590aa
Zachary Williamson <williamson.zachary@outlook.com>
2017-07-08 12:17:42 -0500

    Created examples.py with valid queries for testing/messages.

---
+++ examples.py
@@ -0,0 +1,49 @@
+EXAMPLE_ANIME = """
+    - Death Note anime, 
+    - Dragon ball super anime status,
+    - What is the anime rating of One Punch Man?"""
+
+EXAMPLE_BOOKS = """
+    - book timeline
+    - harry potter book plot
+    - little women book rating"""
+
+EXAMPLE_CONVERSIONS = """
+    - HKD to USD
+    - USD to EUR rate
+    - how much is 100 USD to INR"""
+    
+EXAMPLE_DEFINITIONS = """
+    - define comfort
+    - cloud definition
+    - what does an accolade mean?"""
+    
+EXAMPLE_LYRICS = """
+    - paradise lyrics
+    - lyrics of the song hall of fame
+    - What are the lyrics to see you again?"""
+    
+EXAMPLE_MOVIES = """
+    - batman movie
+    - iron man 2 movie plot
+    - What is the rating of happiness movie?"""
+    
+EXAMPLE_TIME = """
+    - time in new york
+    - india time
+    - time at paris"""
+    
+EXAMPLE_VIDEOS = """
+    - sia videos
+    - videos by eminem
+    - video coldplay"""
+    
+EXAMPLE_WEATHER = """
+    - tell me the weather in London
+    - weather Delhi
+    - What's the weather in Texas?"""
+    
+EXAMPLE_WIKI = """
+    - wikipedia barack
+    - html wiki
+    - who is sachin tendulkar"""

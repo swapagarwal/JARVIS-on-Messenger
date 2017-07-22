6cf3180335ab8c2a4fd45415ab57fe6a15c6ccb6
Swapnil Agarwal <a.swapnil@iitg.ernet.in>
2017-07-22 07:27:39 +0000

    Format music module

--- config.py
+++ config.py
@@ -9,7 +9,6 @@ JOKES_SOURCE_FILE = 'data/jokes.json'
 QUOTES_SOURCE_FILE = 'data/quotes.json'
 
 # Access token files
-<<<<<<< HEAD
 SPOTIFY_TOKEN_FILE = 'spotify_token.json'
 
 # API Keys
@@ -23,4 +22,3 @@ SPOTIFY_API_KEY = '<<SPOTIFY_API_KEY>>'
 TIME_ZONE_DB_API_KEY = '<<TIME_ZONE_DB_API_KEY>>'
 WORDS_API_KEY = '<<WORDS_API_KEY>>'
 YOUTUBE_DATA_API_KEY = '<<YOUTUBE_DATA_API_KEY>>'
-SPOTIFY_API_KEY = '<<SPOTIFY_API_KEY>>'
--- modules/src/music.py
+++ modules/src/music.py
@@ -23,7 +23,6 @@ def api_search(auth_token, search_term):
 def process(input, entities):
     output = {}
     try:
-
         music = entities['music'][0]['value']
 
         with open(config.SPOTIFY_TOKEN_FILE) as token_file:
@@ -60,7 +59,6 @@ def process(input, entities):
         output['input'] = input
         output['output'] = template.get_message()
         output['success'] = True
-	
     except:
         error_message = 'I couldn\'t find any music matching your query.'
         error_message += '\nPlease ask me something else, like:'
@@ -69,5 +67,4 @@ def process(input, entities):
         error_message += '\n  - play hotel california'
         output['error_msg'] = TextTemplate(error_message).get_message()
         output['success'] = False
-    
     return output

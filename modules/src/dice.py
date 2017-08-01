e63312866bdd8b24abd7b9a4ef11fca62123f3b9
astroengisci <legoboy113@yahoo.com>
2017-08-01 11:18:06 -0500

    Dice module extension
    Added the following dice:
    -d4
    -d8
    -d10
    -d12
    -d20
    -d% (d100)

--- modules/src/dice.py
+++ modules/src/dice.py
@@ -2,6 +2,7 @@ import random
 
 import modules
 from templates.attachment import AttachmentTemplate
+from templates.text import TextTemplate
 from templates.quick_replies import add_quick_reply
 
 dice_sides = {
@@ -14,13 +15,32 @@ dice_sides = {
 }
 
 
-def process(input, entities=None):
-    message = AttachmentTemplate(dice_sides[random.randint(1, 6)], type='image').get_message()
-    message = add_quick_reply(message, 'Roll again!', modules.generate_postback('dice'))
-    message = add_quick_reply(message, 'Flip a coin.', modules.generate_postback('coin'))
-    output = {
-        'input': input,
-        'output': message,
-        'success': True
-    }
+def process(input, entities):
+    try:
+        message = None
+        dice = entities['dice'][0]['value']
+        if (dice == "d6"):
+            message = AttachmentTemplate(dice_sides[random.randint(1, 6)], type='image').get_message()
+        else:
+            sides = int(dice[1:])
+            message = TextTemplate("I rolled a " + dice + " and got " + str(random.randint(1, sides))).get_message()
+        message = add_quick_reply(message, 'Roll again!', modules.generate_postback('dice'))
+        message = add_quick_reply(message, 'Flip a coin.', modules.generate_postback('coin'))
+        output = {
+            'input': input,
+            'output': message,
+            'success': True
+        }
+    except:
+        error =  "I'm sorry, something went wrong when rolling the dice."
+        error += "\nI might not be certain what kind of dice you're trying to roll."
+        error += "\nI can roll a d4, d6, d10, d12, d20, or d100 (d%)."
+        error += "\nPlease phrase your request more like this:"
+        error += "\n  - Roll a d6"
+        error += "\n  - Jarvis, roll a die"
+        error += "\n  - Can you roll a d20?"
+        output = {
+            'error_msg': error,
+            'success': False
+        }
     return output
--- modules/tests/test_dice.py
+++ modules/tests/test_dice.py
@@ -3,6 +3,12 @@ import modules
 
 def test_dice():
     assert ('dice' == modules.process_query('Roll a dice')[0])
+    assert ('dice' == modules.process_query('Roll a die')[0])
     assert ('dice' == modules.process_query('Jarvis, roll a dice')[0])
+    assert ('dice' == modules.process_query('Jarvis, roll a die')[0])
+    assert ('dice' == modules.process_query('Jarvis, roll a d4')[0])
     assert ('dice' == modules.process_query('Can you roll a dice?')[0])
+    assert ('dice' == modules.process_query('Roll a d6')[0])
+    assert ('dice' == modules.process_query('Roll a D6')[0])
+    assert ('dice' == modules.process_query('Roll a D20')[0])
     assert ('dice' != modules.process_query('something random')[0])

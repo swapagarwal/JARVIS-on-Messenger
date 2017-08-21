161a189c13fd33ca13ad4800327365e0b49f40e9
jaskaransarkaria <jaskaran.sarkaria@googlemail.com>
2017-08-21 15:34:56 +0100

    Update what_cocktail.py

--- modules/src/what_cocktail.py
+++ modules/src/what_cocktail.py
@@ -1,2 +1,25 @@
-''' argh what cocktail can i make with these ingredients? search and return a cocktail which has 
+#rewrite the program and then release it onto telegram and messenger!
+''' argh what cocktail can i make with these ingredients? search and return a cocktail which has
 the most matching ingredients'''
+
+def search():
+
+
+def spell_check():
+
+
+
+def print_nice():
+
+
+
+def matches():
+
+
+
+
+cocktails = {"Margarita": "lemon", "tequila", "cointraeu"}
+
+
+base_spirits = ["whiskey", "vodka", "tequila", "gin", "brandy"]
+liqueur_list = ["cointreau", "amaretto"]

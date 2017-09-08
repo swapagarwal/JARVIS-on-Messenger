89c37ea0ba474b4cd8ab0ab8b6d394a836f7435f
Shawn Lee <shawnjzlee.me@gmail.com>
2017-09-08 14:15:21 -0700

    Added cryptocurrency ticker lookup feature

---
+++ modules/src/crypto.py
@@ -0,0 +1,50 @@
+import requests
+
+from templates.text import TextTemplate
+
+
+def process(input, entities):
+    output = {}
+    try:
+        # methods:
+        # getmarkets @param none
+        # getcurrencies @param none
+        # getticker @param market (e.g. BTC-LTC)
+        # getmarketsummaries @param NONE
+        # getmarketsummary @param market
+        # getorderbook @param: market, type (e.g. buy, sell, both)
+        # getmarkethistory @param: market
+
+        market = entities['market'][0]['value'].upper()
+        if 'to' in market:
+            market.replace('to', '-').replace(' ', '')
+
+        r = requests.get('https://bittrex.com/api/v1.1/public/getticker?market=' + market)
+        data = r.json()
+
+        if data['success'] == False:
+            market += '-' + market[:market.find('-')]
+            market = market[market.find('-')+1:]
+            r = requests.get('https://bittrex.com/api/v1.1/public/getticker?market=' + market)
+            data = r.json()
+
+        bid = data['result']['Bid']
+        ask = data['result']['Ask']
+        last = data['result']['Last']
+
+        ticker_details = '%s\n  - Bid: %.8f\n  - Ask: %.8f\n  - Last: %.8f' % (
+        market, bid, ask, last)
+
+        output['input'] = input
+        output['output'] = TextTemplate(ticker_details).get_message()
+        output['success'] = True
+    except:
+        error_message = 'I couldn\'t find that ticker.'
+        error_message += '\nPlease ask me something else, like:'
+        error_message += '\n  - BTC to LTC'
+        error_message += '\n  - BTC-LTC'
+        error_message += '\n  - get ticker for BTC to ETH'
+        error_message += '\n  - get market for BTC-LTC'
+        output['error_msg'] = TextTemplate(error_message).get_message()
+        output['success'] = False
+    return output
---
+++ modules/tests/test_crypto.py
@@ -0,0 +1,17 @@
+import modules
+
+
+def test_crypto():
+    assert ('crypto' == modules.process_query('BTC to LTC')[0])
+    assert ('crypto' == modules.process_query('NAV to BTC')[0])
+    assert ('crypto' == modules.process_query('BTC-ETH')[0])
+
+    intent, entities = modules.process_query('cryptocurrency BTC-ETH')
+    assert ('crypto' == intent)
+    assert ('BTC-ETH' == entities['market'][0]['value'])
+
+    intent, entities = modules.process_query('ticker info for ETH to OMG')
+    assert ('crypto' == intent)
+    assert ('ETH to OMG' == entities['market'][0]['value'])
+
+    assert ('crypto' != modules.process_query('tests invalid input')[0])

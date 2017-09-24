0e1b64bdf98149ff352c0844f9bd7061103b8748
iamblizzard <nipunmittal15.com>
2017-09-24 22:37:01 +0530

    commited

---
+++ detect_drowsiness.py
@@ -0,0 +1,160 @@
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+import playsound
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+import imageio
+import visvis as vv
+
+from gi.repository import Gdk
+#from SimpleCV import Camera
+import sys
+
+def sound_alarm(path):
+	playsound.playsound(path)
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+	return ear
+
+
+def main():
+	args = {}
+	args["shape_predictor"] = "/home/rock19/Desktop/drowsiness-detection/shape_predictor_68_face_landmarks.dat"
+	args["alarm"] = "/home/rock19/Desktop/drowsiness-detection/alarm.wav"
+	 
+	EYE_AR_THRESH = 0.26
+	EYE_AR_CONSEC_FRAMES = 20
+
+	COUNTER = 0
+	ALARM_ON = False
+
+	print("[INFO] loading facial landmark predictor...")
+	detector = dlib.get_frontal_face_detector()
+	predictor = dlib.shape_predictor(args["shape_predictor"])
+
+	# grab the indexes of the facial landmarks for the left and
+	# right eye, respectively
+	(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+	(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+	# start the video stream thread
+	print("[INFO] starting video stream thread...")
+
+
+	#vs = cv2.VideoCapture('<video0>') #26582
+	#cv2.namedWindow("Window")
+	#if (vs.isOpened() == False): 
+	#  print("Error opening video stream or file")
+	  
+	reader = imageio.get_reader('<video0>')
+	#t = vv.imshow(reader.get_next_data(), clim=(0, 255))
+
+	time.sleep(1.0)  # If you don't wait, the image will be dark
+
+	i=1
+	# loop over frames from the video stream
+	while True:
+
+		#var, frame = vs.read()
+		#frame = cv2.imread('/home/rock19/Desktop/new/Pictures%d.jpg' % i)
+			
+		for frame in reader:
+
+			#vv.processEvents()
+			#t.SetData(frame)
+
+			frame = imutils.resize(frame, width=450)
+			gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+			
+			# detect faces in the grayscale frame
+			rects = detector(gray, 0)
+
+			# loop over the face detections
+			for rect in rects:
+				# determine the facial landmarks for the face region, then
+				# convert the facial landmark (x, y)-coordinates to a NumPy
+				# array
+				shape = predictor(gray, rect)
+				shape = face_utils.shape_to_np(shape)
+
+				# extract the left and right eye coordinates, then use the
+				# coordinates to compute the eye aspect ratio for both eyes
+				leftEye = shape[lStart:lEnd]
+				rightEye = shape[rStart:rEnd]
+				leftEAR = eye_aspect_ratio(leftEye)
+				rightEAR = eye_aspect_ratio(rightEye)
+
+				# average the eye aspect ratio together for both eyes
+				ear = (leftEAR + rightEAR) / 2.0
+
+				# compute the convex hull for the left and right eye, then
+				# visualize each of the eyes
+				leftEyeHull = cv2.convexHull(leftEye)
+				rightEyeHull = cv2.convexHull(rightEye)
+				cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+				cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+				# check to see if the eye aspect ratio is below the blink
+				# threshold, and if so, increment the blink frame counter
+				if ear < EYE_AR_THRESH:
+					COUNTER += 1
+
+					# if the eyes were closed for a sufficient number of
+					# then sound the alarm
+					if COUNTER >= EYE_AR_CONSEC_FRAMES:
+						# if the alarm is not on, turn it on
+						if not ALARM_ON:
+							ALARM_ON = True
+
+							# sound played in the background
+							if args["alarm"] != "":
+								th = Thread(target=sound_alarm,
+									args=(args["alarm"],))
+								th.deamon = True
+								th.start()
+
+						# draw an alarm on the frame
+
+						cv2.putText(frame, "DROWSINESS ALERT!", (200, 30),
+							cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+				# otherwise, the eye aspect ratio is not below the blink
+				# threshold, so reset the counter and alarm
+				else:
+					COUNTER = 0
+					ALARM_ON = False
+
+				# draw the computed eye aspect ratio on the frame to help
+				# with debugging and setting the correct eye aspect ratio
+				# thresholds and frame counters
+				cv2.putText(frame, "EAR: {:.2f}".format(ear), (10, 30),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+		 
+			# show the frame
+			cv2.imshow("Frame", frame)
+			key = cv2.waitKey(1) & 0xFF
+			i += 1
+
+		break
+
+	# do a bit of cleanup
+	cv2.destroyAllWindows()
+	if vs is True:
+		vs.stop()
--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,9 @@
 import json
 import os
+import detect_drowsiness
+import random
+import emoji
+from unidecode import unidecode
 
 import requests
 from flask import Flask, request
@@ -10,6 +14,12 @@ import modules
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN', config.ACCESS_TOKEN)
 VERIFY_TOKEN = os.environ.get('VERIFY_TOKEN', config.VERIFY_TOKEN)
 
+dict = {}
+
+dict["'\U0001f604'"] = ['Good to see you happy', 'Keep Smiling', 'Happy faces are the best']
+
+
+
 app = Flask(__name__)
 
 
@@ -18,8 +28,18 @@ def about():
     return 'Just A Rather Very Intelligent System, now on Messenger!'
 
 
+@app.route('/camera')
+def run():
+	return detect_drowsiness.main()
+
+
 @app.route('/process/')
 def process():
+    
+    x =  request.args.get('q').encode('unicode-escape')    # read emoji and convert it to its unicode
+    
+    print dict[x][random.randint(0,len(dict[x])-1)]
+    #print request.args.get('q').decode('unicode-escape')    # read unicode and convert it to its emoji
     return json.dumps(modules.process_query(request.args.get('q')))
 
 
@@ -31,12 +51,29 @@ def search():
 @app.route('/webhook/', methods=['GET', 'POST'])
 def webhook():
     if request.method == 'POST':
+
         data = request.get_json(force=True)
         messaging_events = data['entry'][0]['messaging']
         for event in messaging_events:
             sender = event['sender']['id']
             message = None
+
+            if(request.args.get('q').encode('unicode-escape') in dict):
+                
+                x = request.args.get('q').encode('unicode-escape')
+                payload = {
+                    'recipient': {
+                        'id': sender
+                    },
+                    'message': dict[x][random.randint(0,len(dict[x])-1)]
+                }
+                r = requests.post('https://graph.facebook.com/v2.6/me/messages', params={'access_token': ACCESS_TOKEN},
+                                  json=payload)
+                return ''
+
             if 'message' in event and 'text' in event['message']:
+
+
                 if 'quick_reply' in event['message'] and 'payload' in event['message']['quick_reply']:
                     quick_reply_payload = event['message']['quick_reply']['payload']
                     message = modules.search(quick_reply_payload, sender=sender, postback=True)

e8b29e5696a90542a126a70b8f6c1c29c8a828e9
xdavidhu <xdavid@protonmail.com>
2017-10-29 12:05:04 +0100

    Added case insensitive replace

--- modules/src/hello.py
+++ modules/src/hello.py
@@ -5,12 +5,12 @@ from templates.text import TextTemplate
 
 def process(input, entities=None):
     greetings = [
-        'Welcome home, {0}...',
-        'All wrapped up here, {0}. Will there be anything else?',
-        '{0}, I think I need to sleep now...',
+        'Welcome home, sir...',
+        'All wrapped up here, sir. Will there be anything else?',
+        'Sir, I think I need to sleep now...',
         'I seem to do quite well for a stretch, and then at the end of the sentence I say the wrong cranberry.',
-        'At your service, {0}.',
-        'Oh hello, {0}!',
+        'At your service, sir.',
+        'Oh hello, sir!',
         'Perhaps, if you intend to visit other planets, we should improve the exosystems.',
     ]
 
@@ -21,13 +21,8 @@ def process(input, entities=None):
         if 'sender' in entities and 'first_name' in entities['sender']:
             personalized = True
             sender_name = entities['sender']['first_name']
-            respone = respone.format(sender_name)
-
-    if not personalized:
-        if respone[:3] == "{0}":
-            respone = respone.format("Sir")
-        else:
-            respone = respone.format("sir")
+            respone = response.replace('Sir', sender_name)
+            respone = response.replace('sir', sender_name)
 
     output = {
         'input': input,

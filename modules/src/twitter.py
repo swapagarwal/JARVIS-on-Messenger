a92a0af0b3c28dd265b45a289448f33aaf476a7b
konstantinbork <konstantin.bork@gmail.com>
2017-11-13 19:13:14 +0100

    Cleanup the Twitter module

--- modules/src/twitter.py
+++ modules/src/twitter.py
@@ -1,15 +1,13 @@
 import os
 import config
 
-import requests
-import requests_cache
-
 from TwitterSearch import *
 
 TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY', config.TWITTER_CONSUMER_KEY)
 TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET', config.TWITTER_CONSUMER_SECRET)
 TWITTER_ACCESS_TOKEN = os.environ.get('TWITTER_ACCESS_TOKEN', config.TWITTER_ACCESS_TOKEN)
 TWITTER_ACCESS_TOKEN_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET', config.TWITTER_ACCESS_TOKEN_SECRET)
+
 
 def process(input, entities=None):
     keyword = None
@@ -21,9 +19,9 @@ def process(input, entities=None):
 
     output = {}
     try:
-        response = None
         response = api_search(keyword)
-        message = 'Okay, here are some tweets about ' + keyword + '<br><br>' + response.encode('ascii', 'xmlcharrefreplace')
+        message = 'Okay, here are some tweets about ' + keyword + '<br><br>' + response.encode('ascii',
+                                                                                               'xmlcharrefreplace')
         output['input'] = input
         output['output'] = message
         output['success'] = True
@@ -33,7 +31,6 @@ def process(input, entities=None):
 
 
 def api_search(keyword):
-
     ts = TwitterSearch(
         consumer_key=TWITTER_CONSUMER_KEY,
         consumer_secret=TWITTER_CONSUMER_SECRET,
@@ -41,24 +38,22 @@ def api_search(keyword):
         access_token_secret=TWITTER_ACCESS_TOKEN_SECRET
     )
 
-    tuoFilter = TwitterUserOrder("JARVIS")
-    tuoFilter.set_exclude_replies(True)
-    tuoFilter.set_include_rts(False)
-    tuoFilter.set_contributor_details(False)
+    tuo_filter = TwitterUserOrder("JARVIS")
+    tuo_filter.set_exclude_replies(True)
+    tuo_filter.set_include_rts(False)
+    tuo_filter.set_contributor_details(False)
 
-    tsoFilter = TwitterSearchOrder() # create a TwitterSearchOrder object
-    tsoFilter.set_keywords([keyword])
-    tsoFilter.set_language('en')
-    tsoFilter.set_count(3)
-    tsoFilter.set_include_entities(False) # and don't give us all those entity information
+    tso_filter = TwitterSearchOrder()  # create a TwitterSearchOrder object
+    tso_filter.set_keywords([keyword])
+    tso_filter.set_language('en')
+    tso_filter.set_count(3)
+    tso_filter.set_include_entities(False)  # and don't give us all those entity information
 
     tso = TwitterSearchOrder()
-    tso.set_search_url(tsoFilter.create_search_url() + tuoFilter.create_search_url())
+    tso.set_search_url(tso_filter.create_search_url() + tuo_filter.create_search_url())
 
     result = ''
-    counter = 0
-
-    results = ts.search_tweets(tsoFilter)
+    results = ts.search_tweets(tso_filter)
 
     for tweet in results['content']['statuses']:
         result += tweet['user']['screen_name'] + ' tweeted: ' + tweet['text'] + "<br><br>"

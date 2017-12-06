02396b25dba102d777e292de484a33b351a7a77a
lennyzhang <yunhengz@andrew.cmu.edu>
2017-12-06 18:50:02 -0500

    adds unit test files

---
+++ modules/test_calculator.py
@@ -0,0 +1,83 @@
+import math
+from calculator import process
+
+def test():
+    input = ""
+    entities = {}
+    
+    #case 1: addition/substration
+    entities['operator'] = '+'
+    entities['operand'] = [0, 0]
+    entities['operand'][0] = 32.34
+    entities['operand'][1] = -32.34
+    result = process(input, entities)
+    success = result['success']
+    output = result['output']
+    assert(True == success)
+    assert(0 == output)
+    
+    #case 2: multiply/division
+    entities['operator'] = '/'
+    entities['operand'] = [0, 0]
+    entities['operand'][0] = 32.34
+    entities['operand'][1] = -32.34
+    result = process(input, entities)
+    success = result['success']
+    output = result['output']
+    assert(True == success)
+    assert(-1 == output)
+    
+    #case 3: pow/sqrt
+    entities['operator'] = 'pow'
+    entities['operand'] = [0, 0]
+    entities['operand'][0] = 3
+    entities['operand'][1] = 0
+    result = process(input, entities)
+    success = result['success']
+    output = result['output']
+    assert(True == success)
+    assert(1 == output)
+    
+    #case 4: sin/asin
+    entities['operator'] = 'asin'
+    entities['operand'] = [0, 0]
+    entities['operand'][0] = 0.5
+    result = process(input, entities)
+    success = result['success']
+    output = result['output']
+    assert(True == success)
+    assert(math.pi/6 - output < 0.001)
+    
+    #case 5: cos/acos
+    entities['operator'] = 'cos'
+    entities['operand'] = [0, 0]
+    entities['operand'][0] = math.pi/3
+    result = process(input, entities)
+    success = result['success']
+    output = result['output']
+    assert(True == success)
+    assert(0.5 - output < 0.001)
+    
+    #case 6: tan/atan
+    entities['operator'] = 'tan'
+    entities['operand'] = [0, 0]
+    entities['operand'][0] = math.pi/4
+    result = process(input, entities)
+    success = result['success']
+    output = result['output']
+    assert(True == success)
+    assert(1 - output < 0.001)
+    
+    #case 7: ln/log
+    entities['operator'] = 'ln'
+    entities['operand'] = [0, 0]
+    entities['operand'][0] = math.e
+    result = process(input, entities)
+    success = result['success']
+    output = result['output']
+    assert(True == success)
+    assert(1 - output < 0.001)
+    
+    print("all tests passed")
+
+test()

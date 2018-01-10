632ffeb325b1efff6e2078a5b7f33b993e7d3c3c
Vassilis <vassilisgeorgo@hotmail.com>
2018-01-10 22:49:31 +0200

    add tests in test_cocktail.py

--- modules/tests/test_cocktail.py
+++ modules/tests/test_cocktail.py
@@ -3,4 +3,6 @@ import modules
 
 def test_cocktail():
     assert ('cocktail' == modules.process_query('margarita cocktail')[0])
+    assert ('cocktail' == modules.process_query('mai tai cocktail')[0])
+    assert ('cocktail' == modules.process_query('cocktail martini')[0])
     assert ('cocktail' != modules.process_query('test')[0])

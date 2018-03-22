eb70afd849529c6fea8a5b0fae109637eb345827
Adwait Mathkari <37273956+addyvm22@users.noreply.github.com>
2018-03-22 14:15:36 +0530

    Delete test_leaguetable.py

--- modules/tests/test_leaguetable.py
+++
@@ -1,7 +0,0 @@
-import modules
-
-def test_leaguetable():
-    assert('leaguetable' == modules.process_query('Show me the Premier League Table')[0])
-    assert('leaguetable' == modules.process_query('Show me Italian football team rankings')[0])
-    assert('leaguetable' == modules.process_query('What is the Bundesliga football table?')[0])
-    assert('leaguetable' != modules.process_query('something random just to rename')[0])

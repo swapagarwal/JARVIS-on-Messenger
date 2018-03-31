7c8f4064e0522656d2ed8d00f84ba442446c4c78
barbarkahoufkova <barbara1993@seznam.cz>
2018-03-30 23:53:57 -0400

    Add module

--- modules/src/bmi.py
+++ modules/src/bmi.py
@@ -1,13 +1,46 @@
-
 import config
 import modules
 from templates.text import TextTemplate
+
+def bmi(string):
+	x = string.split()
+	mode = x[2]
+    height = float(x[0])
+    weight = float(x[1])
+	if mode == "imperial":
+		height_sq  = height*height
+		bmi = weight / height_sq * 703
+		bmi_final = int(round(bmi,0))
+		if bmi_final <= 18.5:
+			return ("Your BMI is {} and you belong to category underweight.".format(bmi_final))
+		elif bmi_final < 25:
+			return ("Your BMI is {} and you belong to category normal.".format(bmi_final))
+		elif bmi_final < 30:
+			return ("Your BMI is {} and you belong to category overweight.".format(bmi_final))
+		else: 
+			return ("Your BMI is {} and you belong to category obese.".format(bmi_final))
+	else:
+		heigh_new = heigh/100
+		height_sq  = height*height
+		bmi = weight / height_sq
+		bmi_final = int(round(bmi,0))
+		if bmi_final <= 18.5:
+			return ("Your BMI is {} and you belong to category underweight.".format(bmi_final))
+		elif bmi_final < 25:
+			return ("Your BMI is {} and you belong to category normal.".format(bmi_final))
+		elif bmi_final < 30:
+			return ("Your BMI is {} and you belong to category overweight.".format(bmi_final))
+		else: 
+			return ("Your BMI is {} and you belong to category obese.".format(bmi_final))
+			
+	
+
 
 def process(input, entities=None):
     output = {}
     try:
         
-        text_i_want_returned = do_thing(input) ### TODO: define do_thing()
+        text_i_want_returned = bmi(input) 
         
         message = TextTemplate(text_i_want_returned).get_message()
         output['input'] = input
@@ -18,18 +51,4 @@ def process(input, entities=None):
     return output
 
 
-def bmi(string):
-    x = string.split() #string is divided into two variables
-    height = float(x[0])
-    weight = float(x[1])
-    height_sq = height*height
-    bmi = weight / height_sq * 703 #formula for bmi
-    bmi_final = int(round(bmi,0)) #round bmi
-    if bmi_final == 18.5: #prints to which category you belong including your bmi index
-        return ('Your BMI is {} and you belong to category underweight'.format(bmi_final))
-    elif bmi_final == 25:
-        return ('Your BMI is {} and you belong to category underweight'.format(bmi_final))
-    elif bmi_final == 30:
-        return ('Your BMI is {} and you belong to category underweight'.format(bmi_final))
-    else:
-        return ('Your BMI is {} and you belong to category underweight'.format(bmi_final))
+
--- modules/tests/test_bmi.py
+++ modules/tests/test_bmi.py
@@ -2,7 +2,7 @@ import modules
 
 
 def test_bmi():
-    assert ('bmi' == modules.process_query('Am I ideal? 120 65')[0])
-    assert ('bmi' == modules.process_query('What is my health situation? 110 75')[0])
-    assert ('bmi' == modules.process_query('Do I need to loose weight? 130 100')[0])
+    assert ('bmi' == modules.process_query('bmi Am I ideal? 120 65 imperial')[0])
+    assert ('bmi' == modules.process_query('bmi What is my health situation? 110 75 imperial')[0])
+    assert ('bmi' == modules.process_query('bmi Do I need to loose weight? 165 70 metric')[0])
     assert ('bmi' != modules.process_query('something random')[0])

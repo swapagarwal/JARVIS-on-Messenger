20ec881e7ffcc6f4b888958c8a2d9d1993a8db29
Ashran Bakth <ashran@umich.edu>
2018-04-15 21:34:25 -0400

    Delete test_manga.py

--- modules/tests/test_manga.py
+++
@@ -1,8 +0,0 @@
-import modules
-
-
-def test_manga():
-    assert ('manga' == modules.process_query('Naruto manga')[0])
-    assert ('manga' == modules.process_query('One Piece manga status')[0])
-    assert ('manga' == modules.process_query('Information on Bleach manga')[0])
-    assert ('manga' != modules.process_query('something random')[0])

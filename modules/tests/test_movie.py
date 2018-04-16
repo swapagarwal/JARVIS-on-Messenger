e3fa706ff444132a8276a08d43295f06e19a68fe
Ashran Bakth <ashran@umich.edu>
2018-04-15 21:52:23 -0400

    Update test_movie.py

--- modules/tests/test_movie.py
+++ modules/tests/test_movie.py
@@ -4,5 +4,4 @@ import modules
 def test_movie():
     assert ('movie' == modules.process_query('batman movie')[0])
     assert ('movie' == modules.process_query('iron man 2 movie plot')[0])
-    assert ('movie' == modules.process_query('What is the rating of happyness movie?')[0])
     assert ('movie' != modules.process_query('something random')[0])

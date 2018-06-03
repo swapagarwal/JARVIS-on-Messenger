945b2dd0d3d2db2b40a7b1a8fef80c04da0225e7
KIMSOOJIN97 <37290989+KIMSOOJIN97@users.noreply.github.com>
2018-06-03 22:17:10 +0900

    Update test_news.py

--- modules/tests/test_news.py
+++ modules/tests/test_news.py
@@ -1,8 +1,8 @@
 import modules
 
-
 def test_news():
-    assert ('news' == modules.process_query('news')[0])
-    assert ('news' == modules.process_query('latest news')[0])
-    assert ('news' == modules.process_query('world news')[0])
-    assert ('news' != modules.process_query('something random')[0])
+	assert('news' == modules.process_query('What todays news in Australia?')[0])
+	assert('news' == modules.process_query('Whats the news headline?')[0])
+	assert('news' == modules.process_query('headline, please')[0])
+	assert('news' != modules.process_query('something random')[0])
+

43a9f30ed8d8d8a2ef22800b12509643ba5c8680
choijungp <37467592+choijungp@users.noreply.github.com>
2018-06-03 13:23:57 +0000

    I create "test_foodmenu.py"
    I create "test_foodmenu.py" to add "food menu" module!

--- modules/tests/test_foodmenu.py
+++ modules/tests/test_foodmenu.py
@@ -5,3 +5,4 @@ def test_foodmenu():
     assert ('menu' == modules.process_query('What should I eat?')[0])
     assert ('menu' == modules.process_query('Choose a menu')[0])
     assert ('menu' == modules.process_query('Pick me a menu')[0])
+    assert ('menu' != modules.process_query('something random')[0])

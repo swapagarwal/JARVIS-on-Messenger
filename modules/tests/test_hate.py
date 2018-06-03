38eb696d7e757b9513b4e3a1315864ae0a8ac3cf
limmiok <38207157+limmiok@users.noreply.github.com>
2018-06-03 12:52:17 +0000

    Create test_hate.py
    Create test_hate.py

---
+++ modules/tests/test_hate.py
@@ -0,0 +1,6 @@
+import modules
+
+def test_hate():
+    assert('hate' == module.process_query('i hate you')[0])
+    assert('hate' == module.process_query('Do you hate me?')[0])
+    assert('hate' == module.process_query('something random')[0])

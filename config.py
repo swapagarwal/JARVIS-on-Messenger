ffe62fd17ad6650074c119d220694ce9b9af62c5
KIMSOOJIN97 <37290989+KIMSOOJIN97@users.noreply.github.com>
2018-06-04 23:32:05 +0900

    Update config.py
    add about lucky

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ WIT_AI_ACCESS_TOKEN = 'IKJJJYYVR3X672DHFVS7U7C4L2MQSS2P'
 FACTS_SOURCE_FILE = 'data/facts.json'
 JOKES_SOURCE_FILE = 'data/jokes.json'
 QUOTES_SOURCE_FILE = 'data/quotes.json'
-
+LUCKY_SOURCE_FILE='data/lucky.json'
 # Access token files
 SPOTIFY_TOKEN_FILE = 'tokens/spotify_token.json'
 

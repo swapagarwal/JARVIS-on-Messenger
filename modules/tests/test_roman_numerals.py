e64fc8da2e3126e5646749ffb9a27253e4fce70e
CY3ERW0LF <Jonas.wolff99@gmail.com>
2018-09-13 20:27:26 +0200

    Update test_roman_numerals.py

--- modules/tests/test_roman_numerals.py
+++ modules/tests/test_roman_numerals.py
@@ -1,7 +1,7 @@
 import modules
 
 
-def test_dictionary():
+def test_roman_numerals():
     assert ('roman_numerals' == modules.process_query('write 400 as roman numrals')[0])
     assert ('roman_numerals' == modules.process_query('write 768 in roman number')[0])
     assert ('roman_numerals' == modules.process_query('how do you write 56 in roman numrals')[0])

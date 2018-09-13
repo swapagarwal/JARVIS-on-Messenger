3c9dd56d7dbeafb6aabe4055afe820b3a61b507b
CY3ERW0LF <Jonas.wolff99@gmail.com>
2018-09-13 14:33:01 +0200

    Update test_roman_numerals.py

--- modules/tests/test_roman_numerals.py
+++ modules/tests/test_roman_numerals.py
@@ -5,4 +5,4 @@ def test_dictionary():
     assert ('roman_numerals' == modules.process_query('write 400 as roman numrals')[0])
     assert ('roman_numerals' == modules.process_query('write 768 in roman number')[0])
     assert ('roman_numerals' == modules.process_query('how do you write 56 in roman numrals')[0])
-    assert ('roman_numerals' != modules.process_query('can you tell med what 587 is in roman numbers')[0])
+    assert ('roman_numerals' != modules.process_query('can you tell me how old the roman empire is')[0])

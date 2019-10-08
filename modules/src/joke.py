76fc2f5f2e55aa776d272f8ae85aaa77b2d0f321
halversonmd <halversonmd@gmail.com>
2019-10-08 12:06:15 -0600

    Never tell the same joke twice

--- modules/src/joke.py
+++ modules/src/joke.py
@@ -1,10 +1,24 @@
 import json
-from random import choice
+import requests
+from random import randrange
 
 import config
 import modules
 from templates.quick_replies import add_quick_reply
 from templates.text import TextTemplate
+
+def download_new_joke(joke_idx, jokes_list):
+    r = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'})
+    resp = r.json()
+    tick = 0
+    while 'joke' in resp and tick <= 4:
+        if resp['joke'] not in jokes_list:
+            return resp['joke']
+        else:
+            resp = requests.get('https://icanhazdadjoke.com/', headers={'Accept': 'application/json'}).json()
+            tick += 1
+    else:
+        return ''
 
 
 def process(input, entities=None):
@@ -12,14 +26,25 @@ def process(input, entities=None):
     try:
         with open(config.JOKES_SOURCE_FILE) as jokes_file:
             jokes = json.load(jokes_file)
-            jokes_list = jokes['jokes']
-            message = TextTemplate(choice(jokes_list)).get_message()
-            message = add_quick_reply(message, 'One more!', modules.generate_postback('joke'))
-            message = add_quick_reply(message, 'Show me a quote.', modules.generate_postback('quote'))
-            message = add_quick_reply(message, 'Tell me a fact.', modules.generate_postback('fact'))
-            output['input'] = input
-            output['output'] = message
-            output['success'] = True
+        jokes_list = jokes['jokes']
+        joke_idx = randrange(len(jokes_list))
+        joke = jokes_list[joke_idx]
+        message = TextTemplate(joke).get_message()
+        message = add_quick_reply(message, 'One more!', modules.generate_postback('joke'))
+        message = add_quick_reply(message, 'Show me a quote.', modules.generate_postback('quote'))
+        message = add_quick_reply(message, 'Tell me a fact.', modules.generate_postback('fact'))
+        if 'used_jokes' not in jokes:
+            jokes['used_jokes'] = []
+        jokes['used_jokes'].append(joke)
+        new_joke = download_new_joke(joke_idx, jokes_list)
+        if new_joke:
+            print(new_joke)
+            jokes_list[joke_idx] = new_joke
+        output['input'] = input
+        output['output'] = message
+        output['success'] = True
+        with open(config.JOKES_SOURCE_FILE, 'w') as jokes_file:
+            jokes_file.write(json.dumps(jokes, indent=2))
     except:
         output['success'] = False
     return output
--- modules/tests/test_joke.py
+++ modules/tests/test_joke.py
@@ -1,4 +1,6 @@
 import modules
+import config
+import json
 
 
 def test_joke():
@@ -6,3 +8,6 @@ def test_joke():
     assert ('joke' == modules.process_query('Do you know a joke?')[0])
     assert ('joke' == modules.process_query('random jokes')[0])
     assert ('joke' != modules.process_query('something random')[0])
+    with open(config.JOKES_SOURCE_FILE) as jokes_file:
+        jokes = json.load(jokes_file)
+        assert len(jokes['jokes']) == len(set(jokes['jokes']))

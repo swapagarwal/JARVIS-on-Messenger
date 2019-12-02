95e4392916df75ccd534ed358b8253277a24c153
advay26 <advay.tandon@gmail.com>
2019-12-01 21:37:29 -0500

    Improve feature

--- modules/src/fact.py
+++ modules/src/fact.py
@@ -1,25 +1,26 @@
-import json
-from random import choice
-
-import config
 import modules
 from templates.quick_replies import add_quick_reply
+import requests
 from templates.text import TextTemplate
 
 
 def process(input, entities=None):
     output = {}
     try:
-        with open(config.FACTS_SOURCE_FILE) as facts_file:
-            facts = json.load(facts_file)
-            facts_list = facts['facts']
-            message = TextTemplate(choice(facts_list)).get_message()
-            message = add_quick_reply(message, 'Another fact!', modules.generate_postback('fact'))
-            message = add_quick_reply(message, 'Tell me a joke.', modules.generate_postback('joke'))
-            message = add_quick_reply(message, 'Show me a quote.', modules.generate_postback('quote'))
-            output['input'] = input
-            output['output'] = message
-            output['success'] = True
+        URL = "http://jservice.io/api/random"
+        r = requests.get(url=URL)
+        data = r.json()[0]
+
+        message = "Here's some trivia for you.\n\nQ:\t" + data['question'] + "\nA:\t" + data['answer']
+
+        message = TextTemplate(message).get_message()
+
+        message = add_quick_reply(message, 'Another one!', modules.generate_postback('fact'))
+        message = add_quick_reply(message, 'Tell me a joke.', modules.generate_postback('joke'))
+        message = add_quick_reply(message, 'Show me a quote.', modules.generate_postback('quote'))
+        output['input'] = input
+        output['output'] = message
+        output['success'] = True
     except:
         output['success'] = False
     return output

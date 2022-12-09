748fa8f633d672df00010e160ea7a33781ae6a7d
Arjan Bedi <abedi@andrew.cmu.edu>
2022-12-09 13:55:16 -0500

    Fixed testing bugs and updated testing to python3

--- jarvis.py
+++ jarvis.py
@@ -64,4 +64,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+    app.run(host='localhost', port=int(os.environ.get('PORT', 4000)), debug=True)
